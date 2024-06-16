ec5376693c9fc515acd17b8611afe6aa9cfc50f7
kawogi <github@kai.giebeler.de>
2024-06-16 20:21:39 +0200

    properly output Python errors

--- src/main.rs
+++ src/main.rs
@@ -10,24 +10,44 @@ mod scene;
 
 use std::{fs::read_to_string, path::Path, thread};
 
+use log::{error, info};
 use logging::init_logger;
 use rustpython_vm::{self as vm, Settings};
 
 fn python_runner(source_path: &(impl AsRef<Path> + ToString)) {
     let source = read_to_string(source_path).unwrap();
-    let py_result: vm::PyResult<()> =
-        vm::Interpreter::without_stdlib(Settings::default()).enter(|vm| {
-            let scope = vm.new_scope_with_builtins();
-            let code_obj = vm
-                .compile(&source, vm::compiler::Mode::Exec, source_path.to_string())
-                .map_err(|err| vm.new_syntax_error(&err, Some(&source)))?;
+    let path_string = source_path.as_ref().display().to_string();
 
-            vm.run_code_obj(code_obj, scope)?;
+    let settings = Settings::default().with_path(path_string.clone());
 
-            Ok(())
-        });
+    let interpreter = vm::Interpreter::with_init(settings, |_vm| {
+        // TODO add native modules
+        // vm.add_native_module(name, module)
+    });
 
-    py_result.unwrap();
+    interpreter.enter(|vm| {
+        let scope = vm.new_scope_with_builtins();
+        let compile = vm.compile(&source, vm::compiler::Mode::Exec, path_string);
+
+        match compile {
+            Ok(py_code) => match vm.run_code_obj(py_code, scope) {
+                Ok(code_result) => {
+                    info!("Success: {code_result:?}");
+                }
+                Err(exception) => {
+                    let mut output = String::new();
+                    vm.write_exception(&mut output, &exception).unwrap();
+                    error!("Syntax error: {output}");
+                }
+            },
+            Err(err) => {
+                let exception = vm.new_syntax_error(&err, Some(&source));
+                let mut output = String::new();
+                vm.write_exception(&mut output, &exception).unwrap();
+                error!("Runtime error: {output}");
+            }
+        }
+    });
 }
 
 fn main() {
@@ -37,6 +57,7 @@ fn main() {
     let python_tread = thread::spawn(move || python_runner(&source_path));
 
     pollster::block_on(framework::start("demo scene".into()));
+    // FIXME on Windows the window will still be unresponsively lingering until the control was given back to the OS (maybe a bug in `winit`)
 
     python_tread.join().unwrap();
 }
