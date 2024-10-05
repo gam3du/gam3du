36456909ddfa279c72fa0e7cf8391702153532a5
kawogi <github@kai.giebeler.de>
2024-10-05 22:35:57 +0200

    fix typo

--- engines/robot/src/plugin.rs
+++ engines/robot/src/plugin.rs
@@ -7,7 +7,7 @@ pub use native::NativePlugin;
 pub use python::PythonPlugin;
 
 pub trait Plugin {
-    // TODO try changing signature to `&mut Gamestate`
+    // TODO try changing signature to `&mut GameState`
     fn init(&mut self, game_state: &mut GameState);
 
     fn update(&mut self, game_state: &mut GameState);
