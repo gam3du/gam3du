ac7f917bbcb556ce0335d5132dc769e8bfef0547
Stefan Lukas <etaloof@gmail.com>
2024-11-10 19:24:27 +0100

    Update python async wasm

--- runtimes/python/wasm/src/lib.rs
+++ runtimes/python/wasm/src/lib.rs
@@ -2,7 +2,15 @@
 #![allow(missing_docs, reason = "TODO")]
 #![expect(clippy::print_stdout, reason = "just a demo")]
 
+use std::future::Future;
+use std::pin::Pin;
+use std::sync::atomic::AtomicBool;
+use std::sync::Arc;
+use std::task::{Context, Poll};
 use wasm_bindgen::prelude::*;
+use wasm_bindgen_futures::js_sys::Promise;
+use wasm_bindgen_futures::{js_sys, JsFuture};
+use web_sys::console::log_1;
 
 #[wasm_bindgen]
 extern "C" {
@@ -11,18 +19,79 @@ extern "C" {
 }
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
+pub fn greet(name: String) -> Promise {
     let mut i = 0;
-    loop {
-        let callback = callback(&format!("Hello, {name}!"));
-        println!("{i} callback returned {callback}");
-        if callback > 0 {
-            return;
+    log_1(&"in greet".into());
+    wasm_bindgen_futures::future_to_promise(async move {
+        log_1(&"in future 1".into());
+        yield_point().await;
+        log_1(&"in future 2".into());
+        loop {
+            let callback = callback(&format!("Hello, {name}!"));
+            log_1(&format!("{i} callback returned {callback}").into());
+            if callback > 0 {
+                log_1(&"in future 3".into());
+                break;
+            }
+            let time = web_time::Instant::now();
+            while time.elapsed() < web_time::Duration::from_secs(1) {
+                // burn cycles
+            }
+            i += 1;
+            yield_point().await;
         }
-        let time = web_time::Instant::now();
-        while time.elapsed() < web_time::Duration::from_secs(1) {
-            // burn cycles
+        log_1(&"in future 4".into());
+        Ok(JsValue::NULL)
+    })
+}
+
+async fn yield_point() {
+    let future: JsFuture =
+        wasm_bindgen_futures::future_to_promise(async { Ok(JsValue::NULL) }).into();
+    future.await.ok();
+}
+
+struct TimeoutFuture {
+    done: Arc<AtomicBool>,
+    id: i32,
+}
+
+impl TimeoutFuture {
+    fn new() -> Self {
+        let window = web_sys::window().unwrap();
+        let done = Arc::new(AtomicBool::new(false));
+        let f = {
+            let done = Arc::clone(&done);
+            move || done.store(true, std::sync::atomic::Ordering::SeqCst)
+        };
+        let id = window
+            .set_timeout_with_callback(
+                &Closure::once(f)
+                    .as_ref()
+                    .unchecked_ref::<js_sys::Function>(),
+            )
+            .unwrap();
+        Self { done, id }
+    }
+}
+
+impl Drop for TimeoutFuture {
+    fn drop(&mut self) {
+        if let Some(window) = web_sys::window() {
+            window.clear_timeout_with_handle(self.id);
         }
-        i += 1;
+    }
+}
+
+impl Future for TimeoutFuture {
+    type Output = Result<JsValue, JsValue>;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        if self.done.load(std::sync::atomic::Ordering::SeqCst) {
+            Poll::Ready(Ok(JsValue::NULL))
+        } else {
+            cx.waker().wake_by_ref();
+            Poll::Pending
+        }
     }
 }
