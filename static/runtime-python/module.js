43c5b69b62e13b8870bc31d4e1b746fb5eab38ff
kawogi <github@kai.giebeler.de>
2024-11-22 01:38:43 +0100

    connect (hopefully) the last message channel

--- static/runtime-python/module.js
+++ static/runtime-python/module.js
@@ -9,14 +9,19 @@ let worker;
 export function start_worker(channel_buffers, on_python_message) {
     console.info(LOG_SRC, "start_worker", channel_buffers);
 
-    // TODO use `on_python_message`
-
     console.info(LOG_SRC, "starting Python Worker");
     worker = new Worker("./runtime-python/worker.js", { type: "module" });
 
     let init_promise = new Promise((resolve, reject) => {
         worker.addEventListener("message", function init_event_handler(message_event) {
             worker.removeEventListener("message", init_event_handler);
+
+            // message event handler forwarding all PythonRequests to the Application
+            function forward_request(message_event) {
+                let message = message_event.data;
+                console.trace(LOG_SRC, "forwarding request from Python to Application", message);
+                on_python_message(message);
+            }
 
             let message = message_event.data;
             console.info(LOG_SRC, "message from RuntimePython worker → main", message_event.data);
@@ -27,8 +32,14 @@ export function start_worker(channel_buffers, on_python_message) {
                     console.info(LOG_SRC, "sending the channel buffers to the PythonRuntime");
                     set_channel_buffers(channel_buffers);
 
+                    console.info(LOG_SRC, "registering message handler for requests");
+                    worker.addEventListener("message", forward_request);
+
                     console.info(LOG_SRC, "starting PythonRuntime (this will block the containing WebWorker until the script completes)");
                     run();
+
+                    console.info(LOG_SRC, "unregistering message handler for requests");
+                    worker.removeEventListener("message", forward_request);
 
                     console.info(LOG_SRC, "resolving init promise");
                     resolve();
